3de0e45e60529257838e2505f7273883252eb928
Pranav Zarekar <zarekarpranav02@gmail.com>
2020-04-24 04:48:35 +0530

    Pokemon game

---
+++ project_pokedex/pokemon/src/Pokecard.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+import './Pokecard.css'
+//const POKE_API = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/";
+const POKE_API = "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/";
+
+let padToThree = (number) => {
+    return number<=999 ? `00${number}`.slice(-3) : number;
+};
+
+class Pokecard extends Component {
+    render() {
+        let imgSrc= `${POKE_API}${padToThree(this.props.id)}.png`;
+        return (
+            <div className="Pokecard">
+                <h1>{this.props.name}</h1>
+                <img src={imgSrc} alt={this.props.name}/>
+                <div>
+                    Type : {this.props.type}
+                </div>
+                <div>
+                    EXP : {this.props.exp}
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Pokecard;
---
+++ total_basic/index.js
@@ -0,0 +1,11 @@
+class Hello extends React.Component{
+    render(){
+        return (
+            <div>
+                <h1>Hey There ...</h1>
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<Hello/>,document.getElementById('root'));
